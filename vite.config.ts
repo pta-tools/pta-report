2d75913e01e67bd294c0bd23883af0468ea1bb0a
Yuta Koshizawa <koher@koherent.org>
2024-05-14 23:35:22 +0900

    basenameに/pta-reportを追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/pta-report/',
     server: {
         port: 10011,
     },
